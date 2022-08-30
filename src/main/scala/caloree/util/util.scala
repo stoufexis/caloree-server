e44cfc3b71a58d900101664acfc762ec66c1e7cf
StefanosTouf <stefetoufe@gmail.com>
2022-08-30 15:18:51 +0300

    Add utils

---
+++ src/main/scala/caloree/util/util.scala
@@ -0,0 +1,49 @@
+package caloree
+
+import org.http4s.circe.CirceEntityCodec.circeEntityEncoder
+import org.http4s.dsl.Http4sDsl
+import org.http4s.{Headers, Response}
+
+import io.circe.syntax.EncoderOps
+import io.circe.{Encoder => CirceEncoder}
+
+import cats.syntax.all._
+import cats.{Applicative, FlatMap, Monad}
+
+import org.typelevel.ci.CIString
+
+package object util {
+  def extractHeaders[A, B](
+      headers: Headers,
+      names: (String, String),
+      f1: String => A,
+      f2: String => B
+  ): Option[(A, B)] = for {
+    hs1 <- headers.get(CIString(names._1))
+    hs2 <- headers.get(CIString(names._2))
+    h1 = hs1.head.value
+    h2 = hs2.head.value
+  } yield (f1(h1), f2(h2))
+
+  implicit class ToResponseOptionSyntax[F[_]: Monad, A: CirceEncoder](o: F[Option[A]]) {
+    private val dsl: Http4sDsl[F] = Http4sDsl[F]
+    import dsl._
+
+    def asResponse: F[Response[F]] = o.flatMap {
+      case Some(value) => Ok(value.asJson)
+      case None        => NotFound()
+    }
+  }
+
+  implicit class ToResponseListSyntax[F[_]: Monad, A: CirceEncoder](o: F[List[A]]) {
+    private val dsl: Http4sDsl[F] = Http4sDsl[F]
+    import dsl._
+
+    def asResponse: F[Response[F]] = o.map(_.asJson).flatMap(Ok(_))
+  }
+
+  implicit class CondSyntax(p: Boolean) {
+    def cond[A, B](right: => A, left: => B): Either[B, A] =
+      Either.cond(p, right, left)
+  }
+}
