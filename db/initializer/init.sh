c69e4a61c6cb1a249577b38a508e2d22fb270d66
StefanosTouf <stefetoufe@gmail.com>
2022-09-04 13:40:18 +0300

    Conditionally initialize data if uninitialized

--- db/initializer/init.sh
+++ db/initializer/init.sh
@@ -1,16 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
-sleep 5
-
-cat \
-  /opt/tables.sql \
-  /opt/static_foods.sql \
-  /opt/static_nutrient.sql \
-  /opt/views.sql \
-  /opt/functions.sql \
-  /opt/static_food_nutrient.sql \
-  /opt/insert_default_user.sql |
+test ! -f /opt/check/has_ran &&
+  sleep 5 &&
+  cat \
+    /opt/tables.sql \
+    /opt/static_foods.sql \
+    /opt/static_nutrient.sql \
+    /opt/views.sql \
+    /opt/functions.sql \
+    /opt/static_food_nutrient.sql \
+    /opt/insert_default_user.sql |
   psql \
     --set=defaultUsername="$DEFAULT_USERNAME" \
     --set=defaultPassword="$DEFAULT_PASSWORD" \
-    -h postgres -p 5432 -U postgres -d postgres --single-transaction
+    -h postgres -p 5432 -U postgres -d postgres --single-transaction &&
+  touch /opt/check/has_ran
