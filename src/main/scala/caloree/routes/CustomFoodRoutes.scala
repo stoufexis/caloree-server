1a884cc886b834fc5fad7189144cdf21ab743de2
StefanosTouf <stefetoufe@gmail.com>
2022-09-04 20:01:12 +0300

    Add POST and DELETE custom food routes

--- src/main/scala/caloree/routes/CustomFoodRoutes.scala
+++ src/main/scala/caloree/routes/CustomFoodRoutes.scala
@@ -1,29 +1,36 @@
 package caloree.routes
 
-import org.http4s.AuthedRoutes
 import org.http4s.dsl.Http4sDsl
+import org.http4s.{AuthedRoutes, EntityDecoder, Response}
 
-import cats.Monad
+import cats.syntax.all._
+import cats.{Monad, MonadThrow}
 
-import caloree.model.Types.{Description, EntityId, Page, UID}
-import caloree.model.{CustomFood, CustomFoodPreview, User}
+import caloree.dto.CustomFoodDto
+import caloree.model.Types.{CFID, Description, EntityId, Page, UID}
+import caloree.model.{CustomFood, CustomFoodPreview, Nutrients, User}
 import caloree.query.Run
 import caloree.util._
 
 import Params._
 
 object CustomFoodRoutes {
-  def routes[F[_]: Monad](
+  def routes[F[_]: MonadThrow: EntityDecoder[*[_], CustomFoodDto]](
       implicit
-      go: Run.Optional[F, (EntityId[CustomFood], UID), CustomFood],
-      gm: Run.Many[F, (Description, UID), CustomFoodPreview]
+      getOne: Run.Optional[F, (CFID, UID), CustomFood],
+      getMany: Run.Many[F, (Description, UID), CustomFoodPreview],
+      add: Run.Unique[F, (UID, Description, Nutrients), Int],
+      delete: Run.Unique[F, CFID, Int]
   ): AuthedRoutes[User, F] = {
     val dsl = Http4sDsl[F]
     import dsl._
 
     AuthedRoutes.of {
-      case GET -> _ :? CustomFoodIdP(id) as u                       => go.run((id, u.id)).asResponse
-      case GET -> _ :? DescriptionP(d) +& PageP(p) +& Limit(l) as u => gm.run((d, u.id), p, l).asResponse
+      case GET -> _ :? CustomFoodIdP(id) as u                       => getOne.run((id, u.id)).asResponse
+      case GET -> _ :? DescriptionP(d) +& PageP(p) +& Limit(l) as u => getMany.run((d, u.id), p, l).asResponse
+      case DELETE -> _ :? CustomFoodIdP(cfid) as _                  => delete.run(cfid) *> Ok()
+      case req @ POST -> _ as u                                     =>
+        req.decode.foldF(a => a.asResponse, cf => add.run((u.id, cf.description, cf.nutrients)) *> Ok())
     }
   }
 }
