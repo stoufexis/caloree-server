b74071d56f4226475e39b9f7cd5f65f641417e43
StefanosTouf <stefetoufe@gmail.com>
2022-09-08 17:21:31 +0300

    Change type discriminator in ModifyLog codec

--- src/main/scala/caloree/dto/ModifyLog.scala
+++ src/main/scala/caloree/dto/ModifyLog.scala
@@ -4,7 +4,6 @@ import org.http4s.EntityDecoder
 import org.http4s.circe.jsonOf
 
 import io.circe.generic.extras.Configuration
-import io.circe.generic.extras.auto._
 
 import cats.effect.kernel.Concurrent
 
@@ -15,11 +14,29 @@ import java.time.LocalDate
 sealed trait ModifyLog
 
 object ModifyLog {
+  import io.circe.generic.extras.auto._
+
   case class Add(fid: EFID, amount: Grams, day: LocalDate, minute: Minute)                       extends ModifyLog
   case class Remove(fid: Option[EFID], day: LocalDate, minute: MinuteInterval)                   extends ModifyLog
   case class Modify(fid: Option[EFID], newAmount: Grams, day: LocalDate, minute: MinuteInterval) extends ModifyLog
   case class Undo(fid: Option[EFID], day: LocalDate, minute: MinuteInterval, times: Int)         extends ModifyLog
 
-  implicit val jsonDiscriminator: Configuration = Configuration.default.withDiscriminator("type")
+  implicit val jsonDiscriminator: Configuration = Configuration.default.withDiscriminator("t")
   implicit def modifyLogEntityDecoder[F[_]: Concurrent]: EntityDecoder[F, ModifyLog] = jsonOf[F, ModifyLog]
 }
+
+//object Main extends App {
+//  import io.circe.generic.auto._
+//  import io.circe.parser.decode
+//
+//  println(decode[ModifyLog]("{\"type\": \"Add\" ,\"amount\":200,\"day\":\"2022-09-08\",\"fid\":{\"food_id\":167786},\"minute\":987}"))
+//}
+
+//{
+//  "fid" : {
+//    "food_id" : 1
+//  },
+//  "amount" : 200.0,
+//  "day" : "2022-09-08",
+//  "minute" : 987
+//}
